88c4a52189b4173ce4741b41147127e8b142ff8e
Jayakrishnan <thatcoder.space@gmail.com>
2022-05-31 15:27:19 +0530

    Added node mailer mail sending template

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
+var nodemailer = require('nodemailer');
 
-
-const email-sending-using-nodemailer = async (req, res) => {
-
+const email_sending_using_nodemailer = async (req, res) => {
   // health check
   if (req.params["health"] === "health") {
     res.write(JSON.stringify({success: true, msg: "Health check success"}))
     res.end()
   }
 
-  // Add your code here
-  res.write(JSON.stringify({success: true, msg: `Hello email-sending-using-nodemailer`}))
-  res.end()
+  var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'youremail@gmail.com',
+      pass: 'yourpassword'
+    }
+  });
   
+  var mailOptions = {
+    from: 'youremail@gmail.com',
+    to: 'myfriend@yahoo.com',
+    subject: 'Sending Email using Node.js',
+    text: 'That was easy!'
+  };
+  
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.log(error);
+    } else {
+      res.write(JSON.stringify({success: true, msg: `Hello email-sending-using-nodemailer`}))
+      res.end()
+    }
+  });
 }
 
-export default email-sending-using-nodemailer
+export default email_sending_using_nodemailer
